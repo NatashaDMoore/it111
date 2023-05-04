ea42a12b1068354399b456079f19b68a8e804c50
Natasha <natasha.d.moore@seattlecolleges.edu>
2023-05-04 13:45:28 -0700

    Gallon/Gallons unit correction

--- IdeaProjects/week3/src/LitersToGallons.java
+++ IdeaProjects/week3/src/LitersToGallons.java
@@ -5,21 +5,32 @@ public class LitersToGallons {
 
         double l = 0, gal;                                      // Initialize liters
         int counter = 0;                                        // Initialize the counter
+        String galUnit = "";
 
         for(gal = 1; gal <= 100; gal = gal+4) {
 
             l = gal * 3.785;
-            System.out.printf("$%.2f", l);                      // friendly decimal
-            System.out.print(" Liters = " +gal+ " Gallons");    // remove ln to allow on same line
-            System.out.println();                               // create separate line for next loop data
             counter++;                                          // counter adds by one each loop
 
+            // Space between every 5
             if(counter == 5){
 
                 System.out.println();                           // if counter is 5 print extra line
                 counter = 0;                                    // reset counter for next loop
-
             }
+
+            // Gallon vs Gallons
+            if(gal <= 1) {
+                galUnit = "Gallon";
+            } else {
+                galUnit = "Gallons";
+            }
+
+            // Print must be after the if statements but inside the for loop
+
+            System.out.printf("$%.2f", l);                          // friendly decimal
+            System.out.print(" Liters = " +gal+ " " +galUnit);      // remove ln to allow on same line
+            System.out.println();                                   // create separate line for next loop data
 
         } //end for loop
 
